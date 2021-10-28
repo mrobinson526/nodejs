8c032ea01e10d3e45fdadf6ab332932751736139
Margaret <mrobinson526@gmail.com>
2021-10-28 12:53:47 -0500

    first commit

---
+++ popular-downloader.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+const rp = require('request-promise');
+const url = "https://reddit.com/r/popular.json/*.image";
+const image = ['.jpg', '.png', 'gif'];
+const options = {
+    method: 'GET',
+    url: url,
+    encoding: null
+};
+
+rp('https://reddit.com/r/popular.json');
+if (path.extname("https://reddit.com/r/popular.json/*.image) 
+    require('fs').writeFile('./downloads/id.json'),
+    JSON.stringify(id.json);
+        if (err) {
+            console.error('Write Error');
+}
+
+
